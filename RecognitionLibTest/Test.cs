e9df04099f856e34a499273cf2d9092178a47eb5
andredolbnin <andredolbnin@gmail.com>
2020-09-28 12:17:15 +0300

    Initial drop

---
+++ RecognitionLibTest/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HandwrittenDigitRecognitionLib;
+
+namespace RecognitionLibTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Type a directory with images (for instance, C:/Users/andre/Desktop/dotnet4/OnnxSample) and press ENTER.");
+            Console.WriteLine("If the given directory is incorrect, the app gonna use default one.");
+            string dir = Console.ReadLine();
+            Recognition R = new Recognition();
+            R.Run(dir);
+            Console.WriteLine("\nTesting has passed successfully");
+            return;
+        }
+    }
+}
