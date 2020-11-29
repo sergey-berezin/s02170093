3bf6bea65cf618525e15f74cdb6f67768fb4276e
andredolbnin <andredolbnin@gmail.com>
2020-11-29 22:04:32 +0300

    Task 4 init

---
+++ DigitRecognitionLibrary/DatabaseInfAndTools.cs
@@ -0,0 +1,146 @@
+﻿namespace DigitRecognitionLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ImageObj
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public Blob ImageDetails { get; set; }
+        public float Confidence { get; set; }
+
+        public LabelObj LabelObject { get; set; }
+    }
+
+    public class Blob
+    {
+        public int Id { get; set; }
+        public byte[] Image { get; set; }
+    }
+
+    public class LabelObj
+    {
+        public int Id { get; set; }
+        public int Label { get; set; }
+        public int StatCount { get; set; }
+
+        public ICollection<ImageObj> ImageObjs { get; set; }
+    }
+
+    public class LibraryContext : DbContext
+    {
+        public DbSet<LabelObj> LabelObjs { get; set; }
+        public DbSet<ImageObj> ImageObjs { get; set; }
+        public DbSet<Blob> ImageDetails { get; set; }
+
+        private string dir = @"..\DigitRecognitionLibrary\library.db";
+        //string s = Directory.GetCurrentDirectory();
+        protected override void OnConfiguring(DbContextOptionsBuilder o) => o.UseSqlite("Data Source = " + dir);
+
+        public void ClearDb()
+        {
+            foreach (var item in ImageObjs)
+            {
+                ImageObjs.Remove(item);
+            }
+
+            foreach (var item in ImageDetails)
+            {
+                ImageDetails.Remove(item);
+            }
+
+            foreach (var item in LabelObjs)
+            {
+                LabelObjs.Remove(item);
+            }
+
+            SaveChanges();
+        }
+
+        public Tuple<int, float> FindResults(string path)
+        {
+            Tuple<int, float> tmpInf = null;
+            byte[] img = File.ReadAllBytes(path);
+            ImageObj el;
+            try
+            {
+                el = ImageObjs.First(i => i.Path == path);
+            }
+            catch (InvalidOperationException)
+            {
+                el = null;
+            }
+
+            if (el != null)
+            {
+                Entry(el).Reference(i => i.LabelObject).Load();
+                Entry(el).Reference(i => i.ImageDetails).Load();
+                if (img.Length == el.ImageDetails.Image.Length)
+                {
+                    bool check = true;
+                    for (int i = 0; i < img.Length; i++)
+                    {
+                        if (img[i] != el.ImageDetails.Image[i])
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if (check)
+                    {
+                        tmpInf = new Tuple<int, float>(el.LabelObject.Label, el.Confidence);
+                    }
+                }
+            }
+
+            return tmpInf;
+        }
+
+        public void AddResults(string path, int l, float c)
+        {
+            ImageObj imageObj = new ImageObj();
+            imageObj.Path = path;
+            byte[] img = File.ReadAllBytes(path);
+            imageObj.ImageDetails = new Blob() { Image = img };
+            imageObj.Confidence = c;
+
+            LabelObj el;
+            try
+            {
+                el = LabelObjs.First(i => i.Label == l);
+                Entry(el).Collection(i => i.ImageObjs).Load();
+            }
+            catch (InvalidOperationException)
+            {
+                el = null;
+            }
+
+            if (el == null)
+            {
+                imageObj.LabelObject = new LabelObj() { Label = l, StatCount = 1 };
+                imageObj.LabelObject.ImageObjs = new List<ImageObj>();
+                imageObj.LabelObject.ImageObjs.Add(imageObj);
+
+                ImageObjs.Add(imageObj);
+                LabelObjs.Add(imageObj.LabelObject);
+                ImageDetails.Add(imageObj.ImageDetails);
+            }
+            else
+            {
+                imageObj.LabelObject = el;
+                imageObj.LabelObject.StatCount++;
+                imageObj.LabelObject.ImageObjs.Add(imageObj);
+
+                ImageObjs.Add(imageObj);
+                ImageDetails.Add(imageObj.ImageDetails);
+            }
+
+            SaveChanges();
+        }
+    }
+}
--- RecognitionApp/MainWindow.xaml.cs
+++ RecognitionApp/MainWindow.xaml.cs
@@ -4,35 +4,39 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
-    using System.Runtime.CompilerServices;
+    using System.Net.Http;
     using System.Text;
     using System.Threading;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Forms.VisualStyles;
-    using System.Windows.Ink;
     using System.Windows.Input;
-    using System.Windows.Media;
     using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-    using DigitRecognitionLibrary;
+    using Newtonsoft.Json;
 
     public partial class MainWindow : Window
     {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string url = "https://localhost:44353/rec";
+        private static CancellationTokenSource cts = new CancellationTokenSource();
+
         public static RoutedCommand Start = new RoutedCommand("Start", typeof(MainWindow));
         public static RoutedCommand Stop = new RoutedCommand("Stop", typeof(MainWindow));
         public static RoutedCommand ClearDb = new RoutedCommand("ClearDb", typeof(MainWindow));
+        public static RoutedCommand GetStats = new RoutedCommand("GetStats", typeof(MainWindow));
 
         private bool isDirPathChosen = false;
         private bool isProcessing = false;
         private bool isClearingDb = false;
+        private bool isGettingStats = false;
+
+        private ObservableCollection<ImgInf> ImgCollection;
+        private ObservableCollection<LabelInf> LabelsCollection;
+        private ObservableCollection<OneLabel> OneLabelCollection;
+
+        private string DirPath { get; set; }
 
         public MainWindow()
         {
@@ -49,7 +53,7 @@
             LbOLC.SetBinding(ItemsControl.ItemsSourceProperty, BndLabelsColl);
             for (int i = 0; i < 10; i++)
             {
-                LabelsCollection.Add(new LabelInf() { Label = i, Count = 0, CountInDb = 0 });
+                LabelsCollection.Add(new LabelInf() { Label = i, Count = 0 });
             }
 
             OneLabelCollection = new ObservableCollection<OneLabel>();
@@ -57,39 +61,7 @@
             BndOneLabelColl.Source = OneLabelCollection;
             LbOneLabel.SetBinding(ItemsControl.ItemsSourceProperty, BndOneLabelColl);
 
-            LibContext = new LibraryContext();
-            lockObj = new object();
-        }
-
-        private Recognition R = new Recognition();
-        private ObservableCollection<ImgInf> ImgCollection;
-        private ObservableCollection<LabelInf> LabelsCollection;
-        private ObservableCollection<OneLabel> OneLabelCollection;
-        private LibraryContext LibContext;
-        private object lockObj;
-
-        private string DirPath { get; set; }
-
-        private void OutputHandler(object sender, Prediction pr)
-        {
-            Dispatcher.BeginInvoke(new Action(() =>
-            {
-                ImgInf img = ImgCollection.First(i => i.Path == pr.Path);
-                img.Label = pr.Label;
-                img.Confidence = pr.Confidence;
-
-                LabelInf lbl = LabelsCollection.First(i => i.Label == img.Label);
-                lbl.Count++;
-                lbl.CountInDb++;
-
-                ThreadPool.QueueUserWorkItem(new WaitCallback(param =>
-                {
-                    lock (lockObj)
-                    {
-                        LibContext.AddResults(img.Path, img.Label, img.Confidence);
-                    }
-                }));
-            }));
+            TbStats.Text = "Statistics:";
         }
 
         private void OpenCommandHandler(object sender, ExecutedRoutedEventArgs e)
@@ -104,12 +76,13 @@
 
         private void CanStartCommandHandler(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = isDirPathChosen && !isProcessing && !isClearingDb;
+            e.CanExecute = isDirPathChosen && !isProcessing && !isClearingDb && !isGettingStats;
         }
 
         private void StartCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             isProcessing = true;
+            TbStats.Text = "Statistics:";
             ImgCollection.Clear();
             foreach (LabelInf i in LabelsCollection)
             {
@@ -119,54 +92,71 @@
             OneLabelCollection.Clear();
             ThreadPool.QueueUserWorkItem(new WaitCallback(param =>
             {
-                foreach (string path in Directory.GetFiles(DirPath))
-                {
-                    Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        ImgCollection.Add(new ImgInf()
-                        {
-                            Image = new BitmapImage(new Uri(path)),
-                            Path = path,
-                            Label = -1,
-                            Confidence = -1,
-                        });
-                    }));
-
-                    Tuple<int, float, int> res = LibContext.FindResults(path);
-                    Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        if (res != null)
-                        {
-                            ImgInf img = ImgCollection.First(i => i.Path == path);
-                            img.Label = res.Item1;
-                            img.Confidence = res.Item2;
-
-                            LabelInf lbl = LabelsCollection.First(i => i.Label == img.Label);
-                            lbl.Count++;
-                            lbl.CountInDb = res.Item3;
-
-                            Trace.WriteLine("Reading results from database...");
-                        }
-                        else
-                        {
-                            Recognize(path);
-                            Trace.WriteLine("Recognizing results...");
-                        }
-                    }));
-                }
-
-                isProcessing = false;
+                PostFunc();
             }));
         }
 
-        private void Recognize(string path)
+        private async void PostFunc()
         {
-            ThreadPool.QueueUserWorkItem(new WaitCallback(param =>
+            try
             {
-                R = new Recognition();
-                R.OutputEvent += OutputHandler;
-                R.Run(path);
-            }));
+                var content = new StringContent(JsonConvert.SerializeObject(DirPath), Encoding.UTF8, "application/json");
+                HttpResponseMessage httpResponse;
+                try
+                {
+                    httpResponse = await client.PostAsync(url, content, cts.Token);
+                }
+                catch (HttpRequestException)
+                {
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MessageBox.Show("NO CONNECTION!", "Info");
+                        StopCommandHandler(null, null);
+                    }));
+                    return;
+                }
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    var item = JsonConvert.DeserializeObject<List<TransferFile>>(httpResponse.Content.ReadAsStringAsync().Result);
+                    foreach (var pr in item)
+                    {
+                        await Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            ImgCollection.Add(new ImgInf()
+                            {
+                                Image = ToBitmapImage(Convert.FromBase64String(pr.Image)),
+                                Path = pr.Path,
+                                Label = Convert.ToInt32(pr.Label),
+                                Confidence = (float)Convert.ToDouble(pr.Confidence),
+                            });
+
+                            LabelInf lbl = LabelsCollection.First(i => i.Label == Convert.ToInt32(pr.Label));
+                            lbl.Count++;
+                        }));
+                    }
+
+                    isProcessing = false;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                await Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    MessageBox.Show("RECOGNITION WAS STOPPED!", "Info");
+                }));
+            }
+        }
+
+        private BitmapImage ToBitmapImage(byte[] array)
+        {
+            using var ms = new MemoryStream(array);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
         }
 
         private void CanStopCommandHandler(object sender, CanExecuteRoutedEventArgs e)
@@ -176,26 +166,86 @@
 
         private void StopCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
-            R.StopRecognition();
+            cts.Cancel(false);
+            cts.Dispose();
+            cts = new CancellationTokenSource();
+            isProcessing = false;
         }
 
         private void CanClearDbCommandHandler(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = !isProcessing;
+            e.CanExecute = !isProcessing && !isGettingStats;
         }
 
         private void ClearDbCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
+            isClearingDb = true;
             ThreadPool.QueueUserWorkItem(new WaitCallback(param =>
             {
-                isClearingDb = true;
-                LibContext.ClearDb();
-                LibContext = new LibraryContext();
-                foreach (LabelInf i in LabelsCollection)
+                try
                 {
-                    i.CountInDb = 0;
+                    var httpResponse = client.DeleteAsync(url).Result;
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        TbStats.Text = "Statistics:";
+                    }));
                 }
+                catch (AggregateException)
+                {
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MessageBox.Show("NO CONNECTION!", "Info");
+                    }));
+                }
+
                 isClearingDb = false;
+            }));
+        }
+
+        private void CanGetStatsCommandHandler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !isProcessing && !isClearingDb;
+        }
+
+        private void GetStatsCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            isGettingStats = true;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(param =>
+            {
+                try
+                {
+                    var httpResponse = client.GetAsync(url).Result;
+                    var stats = JsonConvert.DeserializeObject<string[]>(httpResponse.Content.ReadAsStringAsync().Result);
+                    if (stats.Length == 0)
+                    {
+                        Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            TbStats.Text = "Statistics: database is empty.";
+                        }));
+                    }
+                    else
+                    {
+                        string statsOutput = "Statistics:\n";
+                        for (int i = 0; i < stats.Length; i++)
+                        {
+                            statsOutput += stats[i] + "\n";
+                        }
+
+                        Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            TbStats.Text = statsOutput;
+                        }));
+                    }
+                }
+                catch (AggregateException)
+                {
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MessageBox.Show("NO CONNECTION!", "Info");
+                    }));
+                }
+
+                isGettingStats = false;
             }));
         }
 
@@ -217,16 +267,42 @@
         }
     }
 
-    public class ImgInf: INotifyPropertyChanged
+    public class ImgInf : INotifyPropertyChanged
     {
+        private BitmapImage i;
+        private string p;
         private int l;
         private float c;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public BitmapImage Image { get; set; }
+        public BitmapImage Image
+        {
+            get
+            {
+                return i;
+            }
 
-        public string Path { get; set; }
+            set
+            {
+                i = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Image"));
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                return p;
+            }
+
+            set
+            {
+                p = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Path"));
+            }
+        }
 
         public int Label
         {
@@ -260,7 +336,6 @@
     public class LabelInf : INotifyPropertyChanged
     {
         private int c;
-        private int cdb;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -277,20 +352,6 @@
             {
                 c = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
-            }
-        }
-
-        public int CountInDb
-        {
-            get
-            {
-                return cdb;
-            }
-
-            set
-            {
-                cdb = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CountInDb"));
             }
         }
     }
@@ -314,5 +375,13 @@
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Image"));
             }
         }
+    }
+
+    public class TransferFile
+    {
+        public string Path { get; set; }
+        public string Label { get; set; }
+        public string Confidence { get; set; }
+        public string Image { get; set; }
     }
 }
